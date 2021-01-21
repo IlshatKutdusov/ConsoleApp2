efa6106e245f1e6a0272f343f31454036cc73631
SizeR <vip.sanki007@bk.ru>
2021-01-21 16:13:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "dbdaadabacabbacbbaccbbaccbbddadadac";
+            string s1 = "";
+            for (int i=0; i<s.Length; i++)
+            {
+                if (s[i] == 'a')
+                {
+                    s1 += "00";
+                }
+                if (s[i] == 'b')
+                {
+                    s1 += "01";
+                }
+                if (s[i] == 'c')
+                {
+                    s1 += "10";
+                }
+                if (s[i] == 'd')
+                {
+                    s1 += "11";
+                }
+            }
+            Console.WriteLine(s1);
+            Console.ReadLine();
+        }
+    }
+}
